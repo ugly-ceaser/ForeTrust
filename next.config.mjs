df0cfca9de77ff74c3ba71be67619ec13f139cb8
Ugly Ceaser <74794170+ugly-ceaser@users.noreply.github.com>
2024-11-06 12:21:21 +0100

    working build1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    distDir: 'build',  // Set the build directory to 'build'
+  };
+  
+  export default nextConfig;
+  
