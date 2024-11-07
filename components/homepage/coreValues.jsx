0416c97f15f8cb87d27de29434ed4c85e9a33b51
ugly-ceaser <martins.paraclet@yahoo.com>
2024-11-07 23:17:26 +0100

    fixed the contact error2

--- components/homepage/coreValues.jsx
+++ components/homepage/coreValues.jsx
@@ -5,15 +5,6 @@ import React from 'react';
 const CoreValues = () => {
     return (
         <div className={hero.coreValues}>
-<<<<<<< HEAD
-          <h1 className="mb-2 mt-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">Our Core Values</h1>
-
-            <div className={`${hero.cardContainer} flex flex-wrap justify-center`}>
-                {/* Integrity Card */}
-                <div className="flex flex-col shadow-sm border-slate-200 rounded-lg my-2 mx-2 w-60 md:w-72">
-                    <div className="m-2.5 overflow-hidden rounded-md h-50 flex justify-center items-center">
-                        <img className="w-35 h-20 object-cover" src="./images/integrity.png" alt="Integrity" />
-=======
           <h1 class="mb-4 mt-6 text-4xl font-extrabold leading-none tracking-tight text-white-900 md:text-5xl lg:text-6xl dark:text-dark">Our Core Values</h1>
 
             <div className={hero.cardContainer}>
@@ -21,7 +12,6 @@ const CoreValues = () => {
                 <div class="flex flex-col   shadow-sm border-slate-200 rounded-lg my-6 w-60">
                     <div class="m-2.5 overflow-hidden rounded-md h-50 flex justify-center items-center">
                         <img class="w-full h-full object-cover" src="./images/integrity.png" alt="profile-picture" />
->>>>>>> e177fc3a3d25fbe5348649fbbc5f33b2e88184f0
                     </div>
                     <div class="p-6 text-center">
                         <h4 class="mb-1 text-xl font-semibold text-slate-800">
@@ -35,16 +25,9 @@ const CoreValues = () => {
                     
                 </div>
 
-<<<<<<< HEAD
-                {/* Innovation Card */}
-                <div className="flex flex-col shadow-sm border-slate-200 rounded-lg my-4 mx-2 w-60 md:w-72">
-                    <div className="m-2.5 overflow-hidden rounded-md h-50 flex justify-center items-center">
-                        <img className="w-35 h-20 object-cover" src="./images/innovation.png" alt="Innovation" />
-=======
                 <div class="flex flex-col  shadow-sm  border-slate-200 rounded-lg my-6 w-60">
                     <div class="m-2.5 overflow-hidden rounded-md h-50 flex justify-center items-center">
                         <img class="w-full h-full object-cover" src="./images/innovation.png" alt="profile-picture" />
->>>>>>> e177fc3a3d25fbe5348649fbbc5f33b2e88184f0
                     </div>
                     <div class="p-6 text-center">
                         <h4 class="mb-1 text-xl font-semibold text-slate-800">
@@ -58,16 +41,9 @@ const CoreValues = () => {
                     
                 </div>
 
-<<<<<<< HEAD
-                {/* Sustainability Card */}
-                <div className="flex flex-col shadow-sm border-slate-200 rounded-lg my-4 mx-2 w-60 md:w-72">
-                    <div className="m-2.5 overflow-hidden rounded-md h-50 flex justify-center items-center">
-                        <img className="w-35 h-20 object-cover" src="./images/sustain2.png" alt="Sustainability" />
-=======
                 <div class="flex flex-col  shadow-sm  border-slate-200 rounded-lg my-6 w-60">
                     <div class="m-2.5 overflow-hidden rounded-md h-50 flex justify-center items-center">
                         <img class="w-full h-full object-cover" src="./images/sustain2.png" alt="profile-picture" />
->>>>>>> e177fc3a3d25fbe5348649fbbc5f33b2e88184f0
                     </div>
                     <div class="p-6 text-center">
                         <h4 class="mb-1 text-xl font-semibold text-slate-800">
